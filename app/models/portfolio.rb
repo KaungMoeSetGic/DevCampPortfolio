3b7d0ae4c1cc3a2751b3cfc8ea6a2054dcc48453
KaungMoeSetGic <kaungmoeset@gicjp.com>
2023-06-17 22:05:56 +0630

    Implemented concern for managing image data

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,4 +1,5 @@
 class Portfolio < ApplicationRecord
+    include Placeholder
     validates_presence_of :title, :body, :main_image, :thumb_image
 
     def self.angular # Class method
@@ -7,11 +8,11 @@ class Portfolio < ApplicationRecord
 
     scope :ruby_on_rails_portfolio_items, -> { where(subtitle: 'Ruby on Rails') } # lambda function
 
-    after_initialize :set_defaults # after_initialize do when new method in controller run at the same time
+    after_initialize :set_defaults # after_initialize do when new method in controller run at the same time(callback)
 
     def set_defaults
-        self.main_image ||= "https://placehold.co/600x400"
-        self.thumb_image ||= "https://placehold.co/350x200"
+        self.main_image ||= Placeholder.image_generator(height: '600', width: '400')
+        self.thumb_image ||= Placeholder.image_generator(height: '350', width: '200')
     end
 
     # meaning of self.main_image ||= "https://placehold.co/600x400"
