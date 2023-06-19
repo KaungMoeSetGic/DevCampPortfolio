3e5ad012a503ab7adefe8dfc9ba02bd2cbfc628e
KaungMoeSetGic <kaungmoeset@gicjp.com>
2023-06-19 20:30:12 +0630

    Refactored controller to include concern for source feature

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,10 @@
 class ApplicationController < ActionController::Base
     include DeviseWhitelist
-
+    include SetSource
     # Deleted. Cause it doesn't follow the Single responsibility rule
     # before_action :configure_permitted_parameters, if: :devise_controller?
     # def configure_permitted_parameters
     #     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
     #     devise_parameter_sanitizer.permit(:account_update, keys: [:name])
     # end
-
-    before_action :set_source
-
-    def set_source
-        session[:source] = params[:q] if params[:q]
-    end
 end
