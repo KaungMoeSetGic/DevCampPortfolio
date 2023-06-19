72862e0828d306fdc8fdb759a9b3c16b05686ac9
KaungMoeSetGic <kaungmoeset@gicjp.com>
2023-06-19 21:05:10 +0630

    Refactor strong params for portfolio items

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
     include DeviseWhitelist
     include SetSource
+    
     # Deleted. Cause it doesn't follow the Single responsibility rule
     # before_action :configure_permitted_parameters, if: :devise_controller?
     # def configure_permitted_parameters
