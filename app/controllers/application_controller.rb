446b9f17415db04c96db6ed82e7bd228f68dfcc5
KaungMoeSetGic <kaungmoeset@gicjp.com>
2023-06-19 14:08:23 +0630

    Added controller concerns for devise params

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,11 @@
 class ApplicationController < ActionController::Base
 
-    before_action :configure_permitted_parameters, if: :devise_controller?
+    # Deleted. Cause it doesn't follow the Single responsibility rule
+    # before_action :configure_permitted_parameters, if: :devise_controller?
+    # def configure_permitted_parameters
+    #     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    #     devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+    # end
 
-    def configure_permitted_parameters
-        devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-        devise_parameter_sanitizer.permit(:account_update, keys: [:name])
-    end
+    include DeviseWhitelist
 end
